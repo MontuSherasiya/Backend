acea90d2196f7b47ed0de690b178a107751d7df0
MontuSherasiya <sherasiyamontu@gmail.com>
2026-04-14 16:07:43 +0530

    add tweet controller

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -23,15 +23,6 @@ const generateAccessAndRefereshTokens = async (userId) => {
 }
 
 const registerUser = asyncHandler(async (req, res) => {
-    // get user details from frontend
-    // validation - not empty
-    // check if user already exists: username, email
-    // check for images, check for avatar
-    // upload them to cloudinary, avatar
-    // create user object - create entry in db
-    // remove password and refresh token field from response
-    // check for user creation
-    // return res
 
     const { fullName, email, username, password } = req.body
     //console.log("email: ", email);
@@ -93,12 +84,6 @@ const registerUser = asyncHandler(async (req, res) => {
 })
 
 const loginUser = asyncHandler(async (req, res) => {
-    // req body -> data
-    // username or email
-    //find the user
-    //password check
-    //access and referesh token
-    //send cookie
 
     const { email, username, password } = req.body
     console.log(email);
@@ -106,12 +91,6 @@ const loginUser = asyncHandler(async (req, res) => {
     if (!username && !email) {
         throw new ApiError(400, "username or email is required")
     }
-
-    // Here is an alternative of above code based on logic
-    // if (!(username || email)) {
-    //     throw new ApiError(400, "username or email is required")
-
-    // }
 
     const user = await User.findOne({
         $or: [{ username }, { email }]
@@ -156,7 +135,7 @@ const logoutUser = asyncHandler(async (req, res) => {
         req.user._id,
         {
             $unset: {
-                refreshToken: 1 // this removes the field from document
+                refreshToken: 1
             }
         },
         {
